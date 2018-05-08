06da0900d7b3da1351962f1c71780a414e57823e
Jakub Wodyk <yuriyermakov123@gmail.com>
2018-05-08 18:58:09 +0200

    added new boot.js

---
+++ public/js/boot.js
@@ -0,0 +1,18 @@
+(function(){
+let markup = `<div class="headerMenu">
+  <div id="settings">
+    <a href="#"/>SETTINGS</a>
+  </div>
+  <div id="center">
+    <a href="#"/>DASHBOARD</a>
+    <img src="/arts/alloclogo.png" />
+    <a href="#"/>EXPLORE</a>
+  </div>
+  <div id="friends">
+    <a href="#"/>FRIENDS</a>
+  </div>
+</div>`
+let header = document.querySelector('header')
+header.innerHTML = markup
+// jebacbialych
+})()
